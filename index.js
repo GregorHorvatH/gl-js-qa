869234a5c6dfbb5d704c7d994cf585e4a0dbdb89
Herhel Horvat <herhel.horvat@teaminternational.com>
2022-08-29 09:36:10 +0300

    lesson 1 - without webpack

---
+++ index.js
@@ -0,0 +1,9 @@
+import { sayHello, sum } from './src/js/utils.js';
+
+sayHello();
+
+sum(1, 1);
+sum(1, 10);
+
+// export files from index.js in libs => import them
+// absolute/relative imports (how to set up absolute imports)
---
+++ src/js/utils.js
@@ -0,0 +1,9 @@
+export function sayHello() {
+  console.log('Hello world!');
+}
+
+export const sum = (a, b) => {
+  console.log(a + b);
+};
+
+// export default sayHello;
